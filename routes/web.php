634fcae8fff37ac69d82ef4203e6a9e66b843d1c
sonet1182 <shawn@linkstaff.co.jp>
2024-01-01 14:28:11 +0600

    SMS Send by SMS Service Provider

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,5 +13,16 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
+// Yajra Data Table Start
 Route::get('students', [StudentController::class, 'index']);
 Route::get('students/list', [StudentController::class, 'getStudents'])->name('students.list');
+// Yajra Data Table End
+
+
+//Sent SMS Start
+// Send SMS by HTTP
+Route::get('/sms', [HomeController::class, "sms"]);
+// Send SMS by PHP Script
+Route::get('/sms2', [HomeController::class, "sms2"]);
+//Sent SMS End
